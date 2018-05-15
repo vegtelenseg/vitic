459eba88842de1518f8748ab2d8460ec64ef262e
Siya <dot.netntoz@gmail.com>
2018-05-15 10:34:51 +0200

    Replaced switch

--- app.js
+++ app.js
@@ -14,23 +14,20 @@ app.get('*', (req, res) => {
 
 app.post('*', (req, res) => {
   console.log("Req Body: ", req);
-  const { sessionId, serviceCode, phoneNumber, ussdString } = req.body;
+  const { sessionId, serviceCode, phoneNumber, ussdString, text } = req.body;
   let response = '';
   switch (req.body.ussdString) {
     case '*120*1341*003#':
-      // This is the first request. Note how we start the response with CON
       response = `CON Which game to watch?
     1. Sundowns vs Pirates
-    2. Kaizer Chiefs vs Ajax`;
+    2. Kaizer Chiefgs vs Ajax`;
       return res.send(response);
     case '1':
-      // Business logic for first level response
       response = `CON Choose type of stand to watch from
     1. Grand Stand
     2. Side Stand`;
       return res.send(response);
     case '2':
-      // Business logic for first level response
       response = `CON Side Stand costs R200.00
       1. Buy Ticket
       2. Back to Main Menu`;
